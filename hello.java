70474c588b92915caaaf0adff8247a1f48466d12
ddavid09 <ddawidziak.dd@gmail.com>
2019-02-27 16:00:17 +0100

    powtarzam commit bo nie wiem czem wyjebalo blad

--- hello.java
+++ hello.java
@@ -8,14 +8,9 @@ public class hello {
     }
     //dodaje linike zeby sprawdzic git revert 
     //sprawdze czy dziala dodawanie tresci komentarza w VScode 
-<<<<<<< Updated upstream
-    //a teraz sprawdze ile linijek tekstu mozna tam wpierdolic vscode co gada ze max 72 
-}
-=======
     //a teraz sprawdze ile linijek tekstu mozna tam wpierdolic vscode co gada ze max 72
     
     //dodaje sobie linike w nowej galezi - branch_test_stash
 
     //dodaje kolejna linike tym razem chcialbym aby byla dodana na stos a potem do galezi master
 }   
->>>>>>> Stashed changes

f6b1a1ef5f99fb34de003e736a8db3ed300e0d0e
ddavid09 <ddawidziak.dd@gmail.com>
2019-02-27 15:39:24 +0100

    ciekawe ile linijek tekstu mozna dodac
    fjslkajfslakjfklsajflksdajflksjfklsajflksdjfiurgref;sajdf;klasdjflk;sdjfklsdajflk;sdfjklsdafjklsafjklsa;jfklsajfklsadjfklsdajfksldafjlksadfjsklad;fjsa;lkdfjsa;dlfjsad;lfjs;adlfj;saldfjs;adlfjs;adlfjsad;ljf;sladjf;lksadjf;lkj;lkjlkjkljkljlk;jkl;jkl;jlkjfakl;sdjflsdakfjkldsafjsdlkafjklasdfjlksdajfklsadjflksdajflk;sadjfl;sdajfkls;dafj;lkasdjfl;kasdjf;lsadkjf;lksadjflk;sadjflk;sadjflk;asdjf;lksadjf;lkasdjf;lkdsajflk;sdajfkls;djfkl;sdajfkl;sadjfkl;sadfjkls;adjfk;lsadjfas;lkdjfa;slfjslka;fjls;adkjflsakjfl;ksajfkl;sadjfkl;sadjflk;sadfjl;ksad
    
    fjsadlk;fjd;salj
    
    dupa

--- hello.java
+++ hello.java
@@ -8,4 +8,5 @@ public class hello {
     }
     //dodaje linike zeby sprawdzic git revert 
     //sprawdze czy dziala dodawanie tresci komentarza w VScode 
+    //a teraz sprawdze ile linijek tekstu mozna tam wpierdolic vscode co gada ze max 72 
 }

7b260f195eefc0d150bb40ead75e36191b09a93a
ddavid09 <ddawidziak.dd@gmail.com>
2019-02-27 16:13:17 +0100

    hello.java modified

--- hello.java
+++ hello.java
@@ -5,12 +5,18 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("hello offline git");
-    }
-    //dodaje linike zeby sprawdzic git revert 
+        System.out.println("hello offline git - DEVELOP LINE");
+    } 
     //sprawdze czy dziala dodawanie tresci komentarza w VScode 
     //a teraz sprawdze ile linijek tekstu mozna tam wpierdolic vscode co gada ze max 72
     
-    //dodaje sobie linike w nowej galezi - branch_test_stash
+    //dodaje sobie linike w nowej galezi - branch_test_stash TTO SOBIE ZMODYFIKUJE W DEVELOP 
 
     //dodaje kolejna linike tym razem chcialbym aby byla dodana na stos a potem do galezi master
+
+    //ROZWIJAM PROJEKT TRALA LAL LALALA 
+
+    //JAK PERSKI DYWAAAN 
+
+    //USOWAM LINIE 10 TA DO TESTU GIT REVERT 
 }   

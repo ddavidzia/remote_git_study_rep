0c9b84cb26402aff13e0d32933390ebef4ac69c5
ddavid09 <ddawidziak.dd@gmail.com>
2019-02-27 16:23:50 +0100

    clear all content in hello.java

--- hello.java
+++ hello.java
@@ -1,16 +1,1 @@
-/**
- * hello
- */
-public class hello {
-
-    public static void main(String[] args) {
-        System.out.println("hello offline git");
-    }
-    //dodaje linike zeby sprawdzic git revert 
-    //sprawdze czy dziala dodawanie tresci komentarza w VScode 
-    //a teraz sprawdze ile linijek tekstu mozna tam wpierdolic vscode co gada ze max 72
-    
-    //dodaje sobie linike w nowej galezi - branch_test_stash
-
-    //dodaje kolejna linike tym razem chcialbym aby byla dodana na stos a potem do galezi master
-}   
+NO CONTENT HERE!
